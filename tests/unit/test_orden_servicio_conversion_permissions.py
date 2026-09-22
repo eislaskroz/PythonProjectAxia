63712879cca22293cf215ff9cbee4dca9f1abbc1
Erick Islas <eislaskrozu@gmail.com>
2026-09-22 16:52:49 -0600

    Merge remote-tracking branch 'orign/main'

--- tests/unit/test_orden_servicio_conversion_permissions.py
+++
@@ -1,6 +0,0 @@
-from security.permissions import puede_convertir_levantamiento_a_orden
-
-
-def test_administrador_y_jefe_operaciones_convierten_levantamientos():
-    for tipo in range(1, 7):
-        assert puede_convertir_levantamiento_a_orden({"usu_tipo": tipo}) is (tipo in {1, 2})
